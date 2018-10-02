1174c6328e61e67bda1c266b3b82950be1de307d
nerwin <hippolacroix93@gmail.com>
2018-10-02 10:51:14 -0400

    Add Login and Register pages + validations

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,5 @@
 const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
+const { ExtractJwt } = require('passport-jwt');
 const mongoose = require('mongoose');
 
 const User = mongoose.model('users');
@@ -10,9 +10,9 @@ opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = config.get('jwtKey');
 
 module.exports = (passport) => {
-  passport.use(new JwtStrategy(opts, async (jwt_payload, done) => {
+  passport.use(new JwtStrategy(opts, async (jwtPayload, done) => {
     try {
-      const user = await User.findById(jwt_payload.id);
+      const user = await User.findById(jwtPayload.id);
       if (user) return done(null, user);
       return done(null, false);
     } catch (err) {
